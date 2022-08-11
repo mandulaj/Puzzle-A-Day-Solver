68521ac069d2d2a38a0d26fdd3b88ab9f7bf66dc
Jakub Mandula <jakub@mandula.cz>
2022-08-11 11:06:42 +0200

    Split board half way & use 32bit AVX2 instead of 64bit
    - computes 2 cases instead of 1 per 64bit register

--- src/inc/board.h
+++ src/inc/board.h
@@ -27,10 +27,10 @@ typedef struct solution_t {
 } solution_t;
 
 struct hole_checker {
-  board_t *hole_masks;
-  board_t *hole_crosses;
-  uint64_t num_holes;
-  uint64_t num_blocks;
+  board_t *double_hole_masks;
+  board_t *double_hole_crosses;
+  uint64_t num_double_holes;
+  uint64_t num_double_blocks;
 };
 
 /**
@@ -43,6 +43,6 @@ void print_2_raw(uint64_t pattern1, uint64_t pattern2);
 void init_hole_checker(board_t problem, struct hole_checker *hc);
 void free_hole_checker(struct hole_checker *hc);
 
-bool check_holes(board_t board, struct hole_checker *hc);
+// bool check_holes(board_t board, struct hole_checker *hc);
 bool check_holes_simd(board_t board, const struct hole_checker *hc);
 void print_hole_checker(struct hole_checker *hc);
