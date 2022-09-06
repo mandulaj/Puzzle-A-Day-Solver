c22cde15ceb8a3830390ce981165d2e16d29c25d
Jakub Mandula <jakub@mandula.cz>
2022-09-06 09:23:57 +0200

    Add AVX512 checking and alloc to CACHE_LINE_SIZE

--- src/inc/config.h
+++ src/inc/config.h
@@ -2,12 +2,14 @@
 
 #pragma once
 
-#define USE_SIMD
-#define USE_PARALLEL
+#define USE_SIMD // Use AVX for acceleration
+//#define USE_OLD_SIMD // Use old method of AVX acceleration
+
+#define USE_PARALLEL // Parallel Solution
 #define MAX_PIECES 11
 #define MAX_NUM_SOLUTIONS 1000000
-#define SOLUTIONS_BUFFER_SIZE 2048
-#define SORT_PATTERNS
+#define SOLUTIONS_BUFFER_SIZE 4096
+#define SORT_PATTERNS // Sort patterns based on number of positions
 
 #define CHECK_HOLES
 #define CHECK_DOUBLE_HOLES
@@ -18,3 +20,21 @@
 
 #define ONLY_VALID_DATES
 //#define INCLUDE_WEEKDAYS
+
+#define __AVX2__ 1
+
+#ifdef USE_SIMD
+#if (__AVX512F__ == 1)
+
+// USE AVX512
+#define SIMD_AVX512
+#elif (__AVX2__ == 1)
+
+// Use AVX2
+#define SIMD_AVX2
+#else
+// No AVX detected, default to no SIMD
+#undef USE_SIMD
+#endif
+
+#endif
